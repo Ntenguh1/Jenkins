264ad9e18b773b43b891b3024852ac7a76c3f8c3
Ntenguh1 <77852927+Ntenguh1@users.noreply.github.com>
2021-08-13 13:37:40 -0500

    second commit
    optioanl

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#!/bin/bash
+echo "This is an exmple of a script"
+echo "That is running in a build step"
+echo "This is a Param $PASSED"
+NUM=$(($FIRSTNUM + $SECONDNUM))
+echo "$NUM"
 

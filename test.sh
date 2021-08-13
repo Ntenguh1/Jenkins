043306545ef4316e8cd4443b43e9a2fa58e506cf
Nteng <mutohfrankline@gmail.com>
2021-08-13 15:49:21 -0500

    Revert "5th commit"
    This reverts commit 8e4108a59830d4562c85d6932dad4c56db83f092.

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 echo "This is an exmple of a script"
 echo "That is running in a build step"
 echo "This is a Param $PASSED"
-NUM=(($FIRSTNUM + $SECONDNUM))
+NUM=$(($FIRSTNUM + $SECONDNUM))
 echo "$NUM"
 echo "This is the end"
